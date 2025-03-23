857cea4700c2ef3e40040faca88fe5862daf8bad
hypan599 <hanyingpan@gmail.com>
2025-03-23 12:56:59 -0400

    Remove top row, Remove sm_td for now

--- keyboards/xpandi/spkyv3/rules.mk
+++ keyboards/xpandi/spkyv3/rules.mk
@@ -1,4 +1,6 @@
 # This file intentionally left blank
-DEFERRED_EXEC_ENABLE = yes
+# DEFERRED_EXEC_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
+COMMAND_ENABLE = no
